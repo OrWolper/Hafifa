fed68f6aded66121de0f73995941bc14ae6e704d
OrWolper <orwolper@gmail.com>
2024-02-26 10:33:02 +0200

    t

--- test.py
+++ test.py
@@ -1,5 +1,15 @@
-def main():
+import click
+
+@click.command()
+@click.option('--ten', '-t', help='Tenant ID')
+
+def main(ten = None):
+    """
+       Add new key to the database
+    """
+
     print("hello world")
-    
+    print(ten)
+
 if __name__ == '__main__':
     main()
