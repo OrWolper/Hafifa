10d52338a4502cce4cae09480d64f21a65f0a2e1
OrWolper <orwolper@gmail.com>
2024-02-26 10:47:45 +0200

    t

--- test.py
+++ test.py
@@ -2,14 +2,16 @@ import click
 
 @click.command()
 @click.option('--ten', '-t', help='Tenant ID')
+@click.option('--pen', '-p', help='Penant ID')
 
-def main(ten = None):
+def main(ten = None, pen = None):
     """
        Add new key to the database
     """
 
     print("hello world")
     print(ten)
+    print(pen)
 
 if __name__ == '__main__':
     main()
